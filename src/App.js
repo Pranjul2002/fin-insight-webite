b78d0217e1ad36983238b3e93ab1162fd18f51e3
Pranjul2002 <shivamkhankriyal515@gmail.com>
2024-08-11 11:21:30 +0530

    create about-us

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { Routes, Route } from 'react-router-dom';
+
 import Menu from './Components/Menu/Menu';
 
 import Home from './Pages/Home/Home';
+import AboutUs from './Pages/About-Us/AboutUs'
 
 import Footer from './Components/Footer/Footer'
 
@@ -11,7 +14,13 @@ function App() {
   return (
     <div className="App">
       <Menu/>
-      <Home/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about-us' element={<AboutUs/>}/>
+        <Route path='/' element={<Home/>}/>
+      </Routes>
       <Footer/>
     </div>
   );
